ee8b178cd397737e965c7944f03269cc8eee8a33
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-24 03:57:31 +0000

    2024-09-24 03:57:31

---
+++ Conceptos_de_programacion/02.py
@@ -0,0 +1,21 @@
+
+contraseña_guardada = "palabraClave"
+
+
+intentos_maximos = 3
+intentos = 0
+
+
+while intentos < intentos_maximos:
+    
+    entrada = input("Introduce la contraseña: ")
+    intentos += 1
+    
+    if entrada == contraseña_guardada:
+        print("¡Bienvenido!")
+        break  
+    else:
+        print("Contraseña incorrecta.")
+
+if intentos == intentos_maximos and entrada != contraseña_guardada:
+    print("Has realizado todos los intentos permitidos.")
