abd541a7d01c9bbd9453313304aab20ec625e909
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-29 02:35:21 +0000

    [TheNDCC] Actualización:  - 2024-09-29 02:35:21

--- Conceptos_de_programacion/semana_04/01.py
+++ Conceptos_de_programacion/semana_04/01.py
@@ -21,7 +21,7 @@ while True:
     else:
         print("Entrada inválida. Por favor, ingresa la cantidad de usuario, otra vez.")
 
-for i in numero:
+for i in int(numero):
     mt = 0
     kg = 0
     mt = input(" el valor de la altura metros")
