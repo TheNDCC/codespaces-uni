1992378a385da036bb267c8686622f4a9b1192a6
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-29 05:16:46 +0000

    [TheNDCC] Actualización:  - 2024-09-29 05:16:46

--- Lenguaje_Programacion/semana_04.c
+++ Lenguaje_Programacion/semana_04.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    char palabra[20];
-
-    do{
-        scanf("%s",palabra);
-
-        if (!feof(stdin)){
-            printf("\n%s",palabra);
-        }
-    }while(!feof(stdin));
+    
 }
