43cfa1aa396d90096c3c474d6e72a0981aaa59f9
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-29 05:22:41 +0000

    [TheNDCC] Actualización:  - 2024-09-29 05:22:41

--- Lenguaje_Programacion/semana_04.c
+++ Lenguaje_Programacion/semana_04.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     char nombre1[]="Mi nombre es chelin";
     char nombre2[]="Mi nombre es Tutoriales(???)";
     printf("%s\n", nombre1);
     printf("%s\n", nombre2);
-    
+    printf("Copio lo q tiene nombre1 arriba de nombre2\n");
+    strcpy(nombre2,nombre1);
+    printf("%s\n",nombre2);
+
+
     return 0;
 }
