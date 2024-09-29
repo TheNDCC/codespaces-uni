87564b95c44d59365fd39adfe18ae992ed4bc605
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-29 04:08:22 +0000

    [TheNDCC] Actualización:  - 2024-09-29 04:08:22

--- Conceptos_de_programacion/semana_04/01.py
+++ Conceptos_de_programacion/semana_04/01.py
@@ -4,12 +4,11 @@ def IMC(kg, mt):
 def Clasificacion_IMC(IMC):
     if IMC < 18.5:
         return "bajo peso"
-    elif 18.5 <= IMC <= 24.9:
+    if IMC <= 24.9:
         return "normal"
-    elif 25.0 <= IMC <= 29.9:
+    if IMC <= 29.9: 
         return "sobrepeso"
-    else:
-        return "obesidad"
+    return "obesidad"
 
 def validar_numero_positivo(mensaje):
     while True:
@@ -22,7 +21,6 @@ def validar_numero_positivo(mensaje):
         except ValueError:
             print("Entrada no válida. Por favor, ingrese un número.")
 
-# Solicitar la cantidad de usuarios
 while True:
     entrada = input("¿Cuántos usuarios se les va a calcular el IMC? ")
     if entrada.isdigit() and int(entrada) > 0:
@@ -31,17 +29,13 @@ while True:
     else:
         print("Entrada inválida. Por favor, ingresa la cantidad de usuarios otra vez.")
 
-# Calcular el IMC para cada usuario
 for i in range(numero):
     print(f"\nUsuario {i + 1}:")
     
-    # Validar entrada para la altura y el peso
     mt = validar_numero_positivo("Introduce la altura en metros: ")
     kg = validar_numero_positivo("Introduce el peso en kg: ")
 
-    # Calcular el IMC y clasificar
     indice_imc = IMC(kg, mt)
     clasificacion = Clasificacion_IMC(indice_imc)
     
-    # Mostrar resultados
     print(f"Tu IMC es {indice_imc:.2f}, lo que indica que tienes {clasificacion}.")
