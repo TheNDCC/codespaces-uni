c8ae96f6f513fb4dc76d69feb8c0e01779569a59
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-09-29 05:19:39 +0000

    [TheNDCC] Actualización:  - 2024-09-29 05:19:39

--- Lenguaje_Programacion/semana_04.c
+++ Lenguaje_Programacion/semana_04.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    
+    char nombre1[]="Mi nombre es chelin";
+    char nombre2[]="Mi nombre es Tutoriales(???)";
+    printf("%s\n", nombre1);
+    printf("%s\n", nombre2);
+    return 0;
 }
