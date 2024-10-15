fe02e717f66ddb18cff510bcdde60fb227d062a9
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-10-15 01:06:21 +0000

    [TheNDCC] Actualización:  - 2024-10-15 01:06:21

---
+++ Lenguaje_Programacion/semana_07/03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, mayor, menor;
+
+    printf("¿Cuántos números deseas ingresar? ");
+    scanf("%d", &n);
+
+    int arr[n];  
+
+    printf("Introduce los números enteros:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    mayor = menor = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > mayor) {
+            mayor = arr[i];
+        }
+        if (arr[i] < menor) {
+            menor = arr[i];
+        }
+    }
+
+    printf("El número mayor es: %d\n", mayor);
+    printf("El número menor es: %d\n", menor);
+
+    return 0;
+}
