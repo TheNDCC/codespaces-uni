0122e2418f42a4746e806a1fc2b828ca0ce94335
TheNDCC <129689213+TheNDCC@users.noreply.github.com>
2024-11-05 23:14:38 +0000

    [TheNDCC] Actualización:  - 2024-11-05 23:14:38

--- Conceptos_de_programacion/semana_07/Vehiculo.java
+++ Conceptos_de_programacion/semana_07/Vehiculo.java
@@ -1,15 +1,15 @@
 
 public class Vehiculo {
-    // Atributos de la clase
+    
     private String marca;
     private String modelo;
     private String color;
     private String placa;
     private double tarifa;
 
-    // Constructor vacío
+    
     public Vehiculo() {
-        // Se pueden inicializar los atributos con valores predeterminados si se desea
+        
         this.marca = "";
         this.modelo = "";
         this.color = "";
@@ -17,7 +17,7 @@ public class Vehiculo {
         this.tarifa = 0.0;
     }
 
-    // Constructor con parámetros
+    
     public Vehiculo(String marca, String modelo, String color, String placa, double tarifa) {
         this.marca = marca;
         this.modelo = modelo;
@@ -26,7 +26,7 @@ public class Vehiculo {
         this.tarifa = tarifa;
     }
 
-    // Métodos para mostrar los atributos (Getters)
+    
     public String getMarca() {
         return marca;
     }
@@ -47,15 +47,15 @@ public class Vehiculo {
         return tarifa;
     }
 
-    // Método main para instanciar objetos
+    
     public static void main(String[] args) {
-        // Creación de objeto usando el constructor vacío
+        
         Vehiculo vehiculo1 = new Vehiculo();
 
-        // Creación de objeto usando el constructor con parámetros
+        
         Vehiculo vehiculo2 = new Vehiculo("Toyota", "Corolla", "Rojo", "ABC123", 150.0);
 
-        // Mostrar los valores de los atributos del objeto vehiculo2
+        
         System.out.println("Marca: " + vehiculo2.getMarca());
         System.out.println("Modelo: " + vehiculo2.getModelo());
         System.out.println("Color: " + vehiculo2.getColor());
